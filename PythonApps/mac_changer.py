71bd2c03cd9574a54000c8074a13cf2382681bca
zerobits01 <zerobits01@yahoo.com>
2020-01-23 13:21:13 +0330

    impriving mac changer

--- PythonApps/mac_changer.py
+++ PythonApps/mac_changer.py
@@ -1,28 +1,34 @@
 import argparse
 import re
 import subprocess
+import random
+import string
 
-string = '''
-    author : zerobits01
-    purpose: increasing anonymity
-    email  : zerobits01@yahoo.com
-    gmail  : zerobits0101@gmail.com
-    team   : Paradox-Squad
-    description : simply use this app to change your NIC mac-addr
 
-'''
-simple_usage = "python mac_changer.py -n eth0 -m 00:11:22:33:44:55"
 
-parser = argparse.ArgumentParser(description=string + simple_usage)
+
+
+parser = argparse.ArgumentParser(description='\tauthor : zerobits01\n' 
+    '\tpurpose: increasing anonymity\n'
+    '\temail  : zerobits01@yahoo.com\n'
+    '\tgmail  : zerobits0101@gmail.com\n'
+    '\tteam   : Paradox-Squad\n'
+    '\tdescription : use this app to change your NIC mac-addr\n'
+            '\tthis just work on unix-base OS\n', formatter_class=argparse.RawTextHelpFormatter)
 
 parser.add_argument('-n','--nic',dest='nic',
                         help="network interface card name e.g : eth0,wlan0",
                             type=str, required=True)
 
-parser.add_argument('-m','--mac',dest='mac',
-                        help="new mac-addr for the nic entered, pattern xx:xx:xx:xx:xx:xx",
-                            type=str, required=True)
+newmac = parser.add_mutually_exclusive_group(required=True)
 
+newmac.add_argument('-m','--mac',dest='mac',
+                        help="new mac-addr for the nic entered, pattern xx:xx:xx:xx:xx:xx",
+                            type=str)
+newmac.add_argument('-rm','--random',dest='randomized',
+                        action='store_true',
+                            default=False,
+                                help="randomly generate a new mac")
 args = parser.parse_args()
 
 class InputExcpetion(Exception):
@@ -32,6 +38,16 @@ class InputExcpetion(Exception):
 class SystemException(Exception):
     def __init__(self, msg):
         self.msg = msg
+
+
+
+def randomMac():
+    # to generate a random mac
+    choices = string.digits + string.ascii_lowercase
+    res = [random.choice(choices) for x in range(12)]
+    random_mac = ':'.join([str(res[n]) + str(res[n+1]) for n in (0,2,4,6,8,10)])
+    return random_mac
+
 
 
 def checkArgs():
@@ -47,9 +63,14 @@ def changeMac():
     # changing mac-addr
     global args
     try :
-        subprocess.call(['ifconfig', args.nic, 'down']) 
-        subprocess.call(['ifconfig', args.nic, 'hw', 'ether', args.mac])
-        subprocess.call(['ifconfig', args.nic, 'up'])
+        if args.randomized : 
+            subprocess.call(['ifconfig', args.nic, 'down']) 
+            subprocess.call(['ifconfig', args.nic, 'hw', 'ether', randomMac()])
+            subprocess.call(['ifconfig', args.nic, 'up'])
+        else :
+            subprocess.call(['ifconfig', args.nic, 'down']) 
+            subprocess.call(['ifconfig', args.nic, 'hw', 'ether', args.mac])
+            subprocess.call(['ifconfig', args.nic, 'up'])
     except Exception:
         raise SystemException(msg="[-] couldn't change mac. system problem")
 
