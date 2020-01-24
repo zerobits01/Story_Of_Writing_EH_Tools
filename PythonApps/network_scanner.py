72a8ac67fd45e53182f39afb5f712512ace20082
zerobits01 <zerobits01@yahoo.com>
2020-01-24 23:37:14 +0330

    asyncing the scan job

--- PythonApps/network_scanner.py
+++ PythonApps/network_scanner.py
@@ -9,9 +9,29 @@
 # implement my own net-scanner
 
 # TODO : implementing subnet and pinging other ips
-
 import scapy.all as scapy
 import argparse
+import asyncio
+import functools
+
+
+
+def force_async(fn):
+    '''
+    turns a sync function to async function using threads
+    '''
+    from concurrent.futures import ThreadPoolExecutor
+    import asyncio
+    pool = ThreadPoolExecutor()
+
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        future = pool.submit(fn, *args, **kwargs)
+        return asyncio.wrap_future(future)  # make it awaitable
+
+    return wrapper
+
+
 
 parser = argparse.ArgumentParser(description='''
     author : zerobits01
@@ -25,18 +45,19 @@ parser.add_argument('-r','--range',help='pass the ip or ip range',
                     required=True, type=str)
 
 args = parser.parse_args()
-
+result = None
 def net_scan_auto(ip):
     # using scapy.arping we gonna use arp protocol to find out macs
     scapy.arping(ip)
 
+@force_async
 def net_scan_manually(ip):
     # scapy has implemented ls method for all objects that shows all attrbs
     # packets has two methods show the res : show, summary
     '''
         for being sure that requests are sended to all nodes
         we set the destination mac to broadcast then all nodes will
-        recieve the request and send back a response 
+        recieve the request and send back a response
     '''
     arp_packet = scapy.ARP(pdst=ip) # this sends to IP[s]
     # packet is in tcp/ip layer we need the physical layer an Ethernet-Frame
@@ -51,18 +72,43 @@ def net_scan_manually(ip):
                 srp returns two values (answered, unanswered)
     '''
     nodes, _ = scapy.srp(arp_req, timeout=1, verbose=False) # _ is unanswered packets
-    return [{'ip' : node[1].pdst,'mac' : node[1].hwdst} for node in nodes]
+    global result
+    result = [{'ip' : node[1].pdst,'mac' : node[1].hwdst} for node in nodes]
 
 def print_result(nodes):
+    print()
+    print('ip\t\t\tmac')
+    print('-------------------------------------')
     for node in nodes :
-        print(node['ip'], node['mac'], sep=' => ')
+        print(node['ip'],node['mac'], sep='\t')
+
+async def scan():
+    try :
+        global result
+        print('[!] please wait.')
+        # scan_task = asyncio.create_task(net_scan_manually(args.range))
+        # done, _ = await asyncio.wait(scan_task)
+        await net_scan_manually(args.range)
+        print_result(result)
+        print('[+] done as well.')
+    except Exception as e:
+        print(e)
+        print('[-] sorry something unexpected happened.')
+
+async def printWait():
+    import time
+    global result
+    counter = 0
+    while result is None:
+        time.sleep(0.25)
+        string = '.' if counter == 0 else '..' if counter == 1 else '...'
+        print('\b\r[!]sending requests' + string + ' ', end='')
+        counter = counter + 1 if counter <= 2 else 0
+
+async def main():
+    await asyncio.gather(scan(),printWait())
 
 # net_scan_auto('192.168.1.1/24')
 if __name__ == '__main__':
-    try :
-        print('[!] please wait.')
-        nodes = net_scan_manually(args.range)
-        print_result(nodes)
-        print('[+] done as well.')
-    except Exception :
-        print('[-] sorry something unexpected happened.')
+    asyncio.run(main())
+    # net_scan_auto(args.range)
