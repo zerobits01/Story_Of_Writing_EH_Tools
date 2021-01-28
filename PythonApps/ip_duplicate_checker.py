bd6458c56c8b631cd4ec980c8b46add55d515818
zerobits01 <zerobits0101@gmail.com>
2021-01-28 10:40:01 +0330

    adding ip duplicate checker, test failed :(

--- PythonApps/ip_duplicate_checker.py
+++ PythonApps/ip_duplicate_checker.py
@@ -6,11 +6,31 @@
 '''
 
 import argparse
-import scapy.all as scapy
+import scapy
+import scapy.all as net_helper
 import subprocess
 import re
 import time
 import sys
+from netaddr import IPNetwork
+import asyncio
+import functools
+from concurrent.futures import ThreadPoolExecutor
+
+
+def force_async(fn):
+    '''
+        turns a sync function to async function using threads
+    '''
+    pool = ThreadPoolExecutor()
+
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        future = pool.submit(fn, *args, **kwargs)
+        return asyncio.wrap_future(future)  # make it awaitable
+
+    return wrapper
+
 
 
 parser = argparse.ArgumentParser(description='''
@@ -28,31 +48,90 @@ parser.add_argument('-r','--range',type=str,
 
 args = parser.parse_args()
 
-def getMac(ip):
+
+class Validator(object):
+
+    @staticmethod
+    def validateIFace(iface):
+        """checks if input interface is existing or not
+        """
+        command = ["ip", "addr", "show", iface]
+        try:
+            grepOut = subprocess.check_output(' '.join(command), shell=True)
+            out_string = grepOut.decode()
+            # sample inet 192.168.1.158
+            get_ip_regex = re.compile(r"inet (?P<ip_addr>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})")
+            return get_ip_regex.search(out_string).group("ip_addr")                    
+        except subprocess.CalledProcessError as grepexc:
+            print("[!] entered interface doesn't exist")
+            return False
+    
+    
+    @staticmethod
+    def validIPAddress(IP):
+        """
+        :type IP: str
+        :rtype: str
+        """
+        ip_list = []
+        for ip in IPNetwork(IP):
+            ip_list.append(str(ip))
+            
+        return ip_list
+
+
+@force_async
+def getMac(src, ip):
     # getting mac-addr of an entered ip
     try :
-        arp_packet = scapy.ARP(pdst=ip)  # this sends to IP[s]
-        return scapy.sr(arp_packet, timeout=2, verbose=False)[0][0][1].hwsrc
-    except Exception :
+        print(f"checking\t{ip}")
+        # subprocess.check_output(f"sudo arping -c 1 {ip}", shell=True)
+        arp_packet = net_helper.ARP(op=1, psrc=src, pdst=ip)
+        # ans, unans = net_helper.sr(arp_packet, multi=1, timeout=2)
+        net_helper.send(arp_packet, verbose=False)
+        packets = scapy.sendrecv.sniff(count=2, lfilter = lambda x: x.haslayer(scapy.layers.l2.ARP) and x.psrc == ip, timeout=3) 
+        
+        # lfilter = lambda x: x.haslayer(net_helper.ARP) 
+        
+        mac = []
+        
+        for packet in packets:
+            mac.append(packet) # [0][0][1].hwsrc
+        
+        if len(mac) >= 2:
+            print(20*"#")
+            print(f"ip dup on {mac[0].psrc}")
+            for m in mac:
+                print(m.hwsrc)
+            print(20*"#")
+             
+                   
+        # return scapy.sr(arp_packet, timeout=2, verbose=False)[0][0][1].hwsrc
+    except Exception as e:
+        print(e)
         print('[-] unexpected error occurred during getting mac!?!')
         exit(1)
 
-# TODO: check interface existance
 
+# results, unanswered = sr(ARP(op=ARP.who_has, psrc='192.168.1.2', pdst='192.168.1.1'))
+# 1 req two replies, i have to handle this
 
-# TODO: check range format
-
-
-# TODO: creating LAN ip address
-
-
-# TODO: checking mac addresses
-
+requests = []
 
 
 if __name__ == "__main__":
     try:
-        print(args.interface)
-        print(args.range)
+        src = Validator.validateIFace(iface=args.interface)
+        dst = Validator.validIPAddress(IP=args.range)
+        for ip in dst:
+            if ip.__contains__(".0") or ip.__contains__(".255"):
+                continue
+            getMac(src, ip)
+
     except Exception as e:
         print(sys.exc_info()[-1].tb_lineno, e)
+        
+
+        
+# ans, unans = net_helper.srp(net_helper.Ether(dst="ff:ff:ff:ff:ff:ff")/net_helper.ARP(pdst="192.168.1.0/24"))
+# ans.summary(lambda s,r: r.sprintf("%Ether.src% %ARP.psrc%") )
--- shell-apps/duplicate_checker.sh
+++ shell-apps/duplicate_checker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-IPS0=`ip a | grep inet | grep -v inet6 | grep -v 127.0.0.1 | grep eth0  | awk '{print $2}' | cut -d '/' -f1`
 IPS1=`ip a | grep inet | grep -v inet6 | grep -v 127.0.0.1 | grep wlan0 | awk '{print $2}' | cut -d '/' -f1`
 OUTPUT="/tmp/duplicate_ip"
+
 rm -f $OUTPUT
 
-for i in $IPS0;
+for i in $IPS1;
         do
                 arping -q -D -I wlan0 -c 2 $i
-                echo eth0 $i $? >> $OUTPUT
+                echo wlan0 $i $? >> $OUTPUT
         done
 
 DUPIP=`awk '$3 == "1" { print $0 }' $OUTPUT`
@@ -21,5 +21,7 @@ else
         echo CRITICAL: Duplicate IP found: $DUPIP
         exit 2
 fi
+
+# arping -c 1 $DUPIP
 
 # sudo arping -c 5 192.168.1.128
