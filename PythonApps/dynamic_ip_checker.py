f6208f12f849bead9029f0d3de6996f6ece8a55f
zerobits01 <zerobits0101@gmail.com>
2021-01-28 14:18:06 +0330

    dhcp project done! :)

--- PythonApps/dynamic_ip_checker.py
+++ PythonApps/dynamic_ip_checker.py
@@ -23,23 +23,32 @@ args = parser.parse_args()
 
 
 
-cap = pyshark.LiveCapture(interface=args.iface, bpf_filter="port 67 and 68")
 # cap.apply_on_packets(returnSystemMac, timeout=3)    
 
 mac_addr  = re.compile(r"Client MAC address: (?P<mac_addr>\S+)")
 host_name = re.compile(r"Host Name: (?P<host_name>\S+)")
 ip_addr   = re.compile(r"Requested IP Address: (?P<ip_addr>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})")
 
-for pkt in cap:
-    dhcp_hdr = str(pkt.dhcp)
-    m1 = mac_addr.search(dhcp_hdr)
-    m2 = host_name.search(dhcp_hdr)
-    m3 = ip_addr.search(dhcp_hdr)
-    if m1 and m2 and m3:
-        mac    = m1.group("mac_addr")
-        hname  = m2.group("host_name")
-        ipaddr = m3.group("ip_addr")
-        print(hname + ":\t" + mac + "\tgot ip:\t" + ipaddr)
+def check_dhcp():
+    for pkt in cap:
+        dhcp_hdr = str(pkt.dhcp)
+        m1 = mac_addr.search(dhcp_hdr)
+        m2 = host_name.search(dhcp_hdr)
+        m3 = ip_addr.search(dhcp_hdr)
+        if m1 and m2 and m3:
+            mac    = m1.group("mac_addr")
+            hname  = m2.group("host_name")
+            ipaddr = m3.group("ip_addr")
+            print(hname + ":\t" + mac + "\tgot ip:\t" + ipaddr)
+
+
+def check_duplicate():
+    pass
+
+
+if __name__ == "__main__":
+    cap = pyshark.LiveCapture(interface=args.iface, bpf_filter="port 67 and 68")
+    check_dhcp()
 
 # adding monitor mode
 '''
